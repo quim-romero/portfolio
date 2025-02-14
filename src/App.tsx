84c3e05353b4ad9fc80973618c4b9fbd4213d1cd
quim-romero <quimromero22@gmail.com>
2025-02-14 12:45:00 +0100

    refactor: add routing for Projects and future pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Projects from "./pages/Projects";
 
 export default function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/projects" element={<Projects />} />
       </Routes>
     </Router>
   );
