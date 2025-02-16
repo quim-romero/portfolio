40c3bef4adfe9cf7b380279018f843b3ddfc0f50
quim-romero <quimromero22@gmail.com>
2025-02-16 13:45:00 +0100

    feat: scroll to top when opening a project detail page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Projects from "./pages/Projects";
-import ProjectDetail from './pages/ProjectDetail';
+import ProjectDetail from "./pages/ProjectDetail";
+import ScrollToTop from "./components/ScrollToTop";
 
 export default function App() {
   return (
     <Router>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/projects" element={<Projects />} />
