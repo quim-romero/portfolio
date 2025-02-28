1aaee318c5e3916106d7b0cf1bd21d67e3917bd4
quim-romero <quimromero22@gmail.com>
2025-02-28 09:00:00 +0100

    chore: install react-helmet-async for SEO metadata control

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,19 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { HelmetProvider } from "react-helmet-async";
 import "./index.css";
 import App from "./App.tsx";
 import { DarkModeProvider } from "./hooks/DarkModeContext";
 import { LanguageProvider } from "./hooks/LanguageProvider";
 
 createRoot(document.getElementById("root")!).render(
-  <LanguageProvider>
-    <DarkModeProvider>
-      <StrictMode>
-        <App />
-      </StrictMode>
-      ,
-    </DarkModeProvider>
-  </LanguageProvider>
+  <HelmetProvider>
+    <LanguageProvider>
+      <DarkModeProvider>
+        <StrictMode>
+          <App />
+        </StrictMode>
+      </DarkModeProvider>
+    </LanguageProvider>
+  </HelmetProvider>
 );
