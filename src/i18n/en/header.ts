0a4ffdbac717a7cb7f5d34cfb4bd877a209f5dfe
quim-romero <quimromero22@gmail.com>
2025-05-18 11:20:00 +0200

    i18n(en): added header navigation keys (home, projects, about, contact, blog)

---
+++ src/i18n/en/header.ts
@@ -0,0 +1,9 @@
+export default {
+  nav: {
+    home: 'Home',
+    projects: 'Projects',
+    about: 'About',
+    contact: 'Contact',
+    blog: 'Blog',
+  },
+};
---
+++ src/i18n/es/header.ts
@@ -0,0 +1,9 @@
+export default {
+  nav: {
+    home: 'Inicio',
+    projects: 'Proyectos',
+    about: 'Sobre mí',
+    contact: 'Contacto',
+    blog: 'Blog',
+  },
+};
