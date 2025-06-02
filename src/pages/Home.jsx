c85674cc6fb5c970820a2e2591a375c57c322113
quim-romero <quimromero22@gmail.com>
2025-06-02 13:14:47 +0200

    Initial Home page and basic index.html & css

---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <main className="min-h-screen flex items-center justify-center bg-background text-foreground">
+      <h1 className="text-3xl font-semibold">Portfolio Homepage</h1>
+    </main>
+  )
+}
+
+export default Home
