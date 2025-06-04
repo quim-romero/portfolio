1d84f8b40724cf71896d6259c4512e90de3c5938
quim-romero <quimromero22@gmail.com>
2025-06-04 11:18:05 +0200

    Add page transition animation

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,10 +1,25 @@
 import Navbar from './Navbar'
+import { AnimatePresence, motion } from 'framer-motion'
+import { useLocation } from 'react-router-dom'
 
 const Layout = ({ children }) => {
+  const location = useLocation()
+
   return (
     <div className="min-h-screen flex flex-col">
       <Navbar />
-      <main className="flex-1">{children}</main>
+      <AnimatePresence mode="wait" initial={false}>
+        <motion.main
+          key={location.pathname}
+          initial={{ opacity: 0, y: 10 }}
+          animate={{ opacity: 1, y: 0 }}
+          exit={{ opacity: 0, y: -10 }}
+          transition={{ duration: 0.3, ease: 'easeInOut' }}
+          className="flex-1"
+        >
+          {children}
+        </motion.main>
+      </AnimatePresence>
     </div>
   )
 }
