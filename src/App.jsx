48d9ce0e63254e469b209a225c1630e50fb9ca85
quim-romero <quimromero22@gmail.com>
2025-06-05 12:43:28 +0200

    Complete project data for all 7 portfolio cases

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
+import Projects from './pages/Projects'
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/projects" element={<Projects />} />
     </Routes>
   )
 }
