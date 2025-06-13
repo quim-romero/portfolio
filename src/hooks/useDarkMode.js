22343f294d0d0ba685d9c31ca448a1d1457b99ba
quim-romero <quimromero22@gmail.com>
2025-06-13 11:30:54 +0200

    Implement custom useDarkMode hook

---
+++ src/hooks/useDarkMode.js
@@ -0,0 +1,28 @@
+import { useEffect, useState } from 'react'
+
+const useDarkMode = () => {
+  const [theme, setTheme] = useState(() => {
+    if (typeof window !== 'undefined') {
+      return localStorage.getItem('theme') || 'dark'
+    }
+    return 'dark'
+  })
+
+  useEffect(() => {
+    const root = window.document.documentElement
+    if (theme === 'dark') {
+      root.classList.add('dark')
+    } else {
+      root.classList.remove('dark')
+    }
+    localStorage.setItem('theme', theme)
+  }, [theme])
+
+  const toggleTheme = () => {
+    setTheme(theme === 'dark' ? 'light' : 'dark')
+  }
+
+  return [theme, toggleTheme]
+}
+
+export default useDarkMode
